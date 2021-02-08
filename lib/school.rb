621cca5e602645059bf1212226560873b03a2c73
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-08 16:28:30 -0500

    commit tree e26116311ccba4d50d18b164cbb324990a15dbcb

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+    attr_accessor :roster, :student_name, :school_name
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new{ |h,k| h[k] = []}
+    end
+
+    def add_student(student_name, grade)
+        @roster[grade] << student_name
+    end
+    
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+    #     @roster = Hash.new{|grade, name_array| [grade] = (name_array.sort)}
+        new_hash = {}  
+        @roster.sort.each do |grade, array|
+            new_hash[grade] = (array.sort)
+    end
+    new_hash
+end
+end
+
